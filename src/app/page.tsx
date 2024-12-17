c20257c4710177cff6d40550d3097f7f8182c3e3
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2024-12-17 14:24:42 +0500

    fetched strapi data for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import { Button } from "@/components/ui/button";
 
-export default function Home() {
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+  try {
+    const response = await fetch(`${baseUrl}${path}`);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  const { title, description } = strapiData.data;
+
   return (
     <main className="container mx-auto py-6">
-      <Button>Our Cool Button</Button>
+      <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
     </main>
   );
 }
