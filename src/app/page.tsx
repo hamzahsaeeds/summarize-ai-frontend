8ac5620463b7bc77f705536bf7f2a47fc7d24e67
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2024-12-20 00:45:51 +0500

    added homePageQuery and used its response in the JSX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        "layout.hero-section": {
+          populate: {
+            image: {
+              fields: ["url", "alternativeText"],
+            },
+            link: {
+              populate: true,
+            },
+          },
+        },
+      },
+    },
+  },
+});
 
 async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
   try {
-    const response = await fetch(`${baseUrl}${path}`);
+    const response = await fetch(url.href);
     return await response.json();
   } catch (error) {
     console.error(error);
@@ -13,6 +37,7 @@ export default async function Home() {
   const strapiData = await getStrapiData("/api/home-page");
   const { title, description } = strapiData.data;
 
+  // console.dir(strapiData, { depth: null });
   return (
     <main className="container mx-auto py-6">
       <h1 className="text-5xl font-bold">{title}</h1>
