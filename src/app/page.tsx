ef2b157c6ffa7eaf0f0b3f202d9fd110e5f0c64d
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2024-12-21 18:58:00 +0500

    refactored data fetching functions from strapi into a loaders file under data directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,7 @@
-import { FeatureSection } from "@/components/custom/features-section";
-import { HeroSection } from "@/components/custom/hero-section";
-import { getStrapiURL } from "@/lib/utils";
-import qs from "qs";
+import { getHomePageData } from "@/data/loaders";
 
-const homePageQuery = qs.stringify({
-  populate: {
-    blocks: {
-      on: {
-        "layout.hero-section": {
-          populate: {
-            image: {
-              fields: ["url", "alternativeText"],
-            },
-            link: {
-              populate: true,
-            },
-          },
-        },
-        "layout.features-section": {
-          populate: {
-            feature: {
-              populate: true,
-            },
-          },
-        },
-      },
-    },
-  },
-});
+import { HeroSection } from "@/components/custom/hero-section";
+import { FeatureSection } from "@/components/custom/features-section";
 
 const blockComponents = {
   "layout.hero-section": HeroSection,
@@ -35,28 +9,13 @@ const blockComponents = {
 };
 
 function blockRenderer(block: any) {
-  const Component = blockComponents[block.__component as keyof typeof blockComponents];
+  const Component =
+    blockComponents[block.__component as keyof typeof blockComponents];
   return Component ? <Component key={block.id} data={block} /> : null;
 }
 
-async function getStrapiData(path: string) {
-  const baseUrl = getStrapiURL();
-
-  const url = new URL(path, baseUrl);
-  url.search = homePageQuery;
-
-  try {
-    const response = await fetch(url.href);
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export default async function Home() {
-  const strapiData = await getStrapiData("/api/home-page");
+  const strapiData = await getHomePageData();
   const { blocks } = strapiData?.data || [];
-
-  // console.dir(blocks, { depth: null });
   return <main>{blocks.map(blockRenderer)}</main>;
 }
