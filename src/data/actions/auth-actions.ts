4a97e80ea2a70a9a547740d74639b5e969452367
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2024-12-30 12:07:33 +0500

    completed entire login function

--- src/data/actions/auth-actions.ts
+++ src/data/actions/auth-actions.ts
@@ -1,6 +1,6 @@
 "use server";
 import { z } from "zod";
-import { registerUserService } from "../services/auth-service";
+import { loginUserService, registerUserService } from "../services/auth-service";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -68,4 +68,71 @@ export async function registerUserAction(prevState: any, formData: FormData) {
   cookieStore.set("jwt", responseData.jwt, config);
 
   redirect("/dashboard");
+}
+
+const schemaLogin = z.object({
+  identifier: z
+    .string()
+    .min(3, {
+      message: "Identifier must have at least 3 or more characters",
+    })
+    .max(20, {
+      message: "Please enter a valid username or email address",
+    }),
+  password: z
+    .string()
+    .min(6, {
+      message: "Password must have at least 6 or more characters",
+    })
+    .max(100, {
+      message: "Password must be between 6 and 100 characters",
+    }),
+});
+
+export async function loginUserAction(prevState: any, formData: FormData) {
+  const validatedFields = schemaLogin.safeParse({
+    identifier: formData.get("identifier"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      ...prevState,
+      zodErrors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Login.",
+    };
+  }
+
+  const responseData = await loginUserService(validatedFields.data);
+
+  if (!responseData) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: "Ops! Something went wrong. Please try again.",
+    };
+  }
+
+  if (responseData.error) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: "Failed to Login.",
+    };
+  }
+
+  console.log(responseData, "responseData");
+
+  const cookieStore = await cookies();
+  cookieStore.set("jwt", responseData.jwt, config);
+
+  redirect("/dashboard");
+}
+
+export async function logoutAction() {
+  const cookieStore = await cookies();
+  cookieStore.set("jwt", "", { ...config, maxAge: 0 });
+  redirect("/");
 }
