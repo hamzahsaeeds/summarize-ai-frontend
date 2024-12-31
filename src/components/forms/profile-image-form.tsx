469619910b50a45daf24f1ce0d07a7b1a2302fce
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2025-01-01 01:43:49 +0500

    added uploadProfileImageAction along with file services for delete and post

--- src/components/forms/profile-image-form.tsx
+++ src/components/forms/profile-image-form.tsx
@@ -1,15 +1,14 @@
 "use client";
 import React from "react";
-// import { useActionState} from "react";
+import { useFormState } from "react-dom";
 
 import { cn } from "@/lib/utils";
-
-// import { uploadProfileImageAction } from "@/data/actions/profile-actions";
 
 import { SubmitButton } from "@/components/custom/submit-button";
 import ImagePicker from "@/components/custom/image-picker";
 import { ZodErrors } from "@/components/custom/zod-errors";
 import { StrapiErrors } from "@/components/custom/strapi-errors";
+import { uploadProfileImageAction } from "@/data/actions/profile-actions";
 
 interface ProfileImageFormProps {
   id: string;
@@ -31,18 +30,18 @@ export function ProfileImageForm({
   data: Readonly<ProfileImageFormProps>;
   className?: string;
 }) {
-  // const uploadProfileImageWithIdAction = uploadProfileImageAction.bind(
-  //   null,
-  //   data?.id
-  // );
+  const uploadProfileImageWithIdAction = uploadProfileImageAction.bind(
+    null,
+    data?.id
+  );
 
-  // const [formState, formAction] = useActionState(
-  //   uploadProfileImageWithIdAction,
-  //   initialState
-  // );
+  const [formState, formAction] = useFormState(
+    uploadProfileImageWithIdAction,
+    initialState
+  );
 
   return (
-    <form className={cn("space-y-4", className)}>
+    <form className={cn("space-y-4", className)} action={formAction}>
       <div className="">
         <ImagePicker
           id="image"
@@ -50,8 +49,8 @@ export function ProfileImageForm({
           label="Profile Image"
           defaultValue={data?.url || ""}
         />
-        {/* <ZodErrors error={formState?.zodErrors?.image} />
-        <StrapiErrors error={formState?.strapiErrors} /> */}
+        <ZodErrors error={formState?.zodErrors?.image} />
+        <StrapiErrors error={formState?.strapiErrors} />
       </div>
       <div className="flex justify-end">
         <SubmitButton text="Update Image" loadingText="Saving Image" />
