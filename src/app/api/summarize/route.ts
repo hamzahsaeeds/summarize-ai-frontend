39b865c0680133227f3cf3c605f24aa520491d13
hamzahsaeeds <hamzahsaeed23@hotmail.com>
2025-01-10 23:56:19 +0500

    used langchain's openai api instance (not working as its not free)

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,6 +1,53 @@
 import { getAuthToken } from "@/data/services/get-token";
 import { getUserMeLoader } from "@/data/services/get-user-me-loader";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
 import { NextRequest } from "next/server";
+
+const TEMPLATE = `
+INSTRUCTIONS: 
+  For this {text} complete the following steps.
+  Generate the title based on the content provided
+  Summarize the following content and include 5 key topics, writing in first person using normal tone of voice.
+  
+  Write a youtube video description
+    - Include heading and sections.  
+    - Incorporate keywords and key takeaways
+
+  Generate bulleted list of key points and benefits
+
+  Return possible and best recommended key words
+`;
+
+async function generateSummary(content: string, template: string) {
+  const prompt = PromptTemplate.fromTemplate(template);
+
+  const model = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    modelName: process.env.OPENAI_MODEL ?? "gpt-4o-mini",
+    temperature: process.env.OPENAI_TEMPERATURE
+      ? parseFloat(process.env.OPENAI_TEMPERATURE)
+      : 0.7,
+    maxTokens: process.env.OPENAI_MAX_TOKENS
+      ? parseInt(process.env.OPENAI_MAX_TOKENS)
+      : 4000,
+  });
+
+  const outputParser = new StringOutputParser();
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  try {
+    const summary = await chain.invoke({ text: content });
+    return summary;
+  } catch (error) {
+    if (error instanceof Error)
+      return new Response(JSON.stringify({ error: error.message }));
+    return new Response(
+      JSON.stringify({ error: "Failed to generate summary." })
+    );
+  }
+}
 
 export async function POST(req: NextRequest) {
   const user = await getUserMeLoader();
@@ -39,5 +86,15 @@ export async function POST(req: NextRequest) {
     return new Response(JSON.stringify({ error: "Unknown error" }));
   }
 
-  return new Response(JSON.stringify({ transcript: transcriptData }));
+  let summary: Awaited<ReturnType<typeof generateSummary>>;
+
+  try {
+    summary = await generateSummary(transcriptData, TEMPLATE);
+    return new Response(JSON.stringify({ data: summary, error: null }));
+  } catch (error) {
+    console.error("Error processing request:", error);
+    if (error instanceof Error)
+      return new Response(JSON.stringify({ error: error.message }));
+    return new Response(JSON.stringify({ error: "Error generating summary." }));
+  }
 }
